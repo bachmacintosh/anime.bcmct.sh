4b3722958549291ea62b294a478591383b6c0245
Collin Bachman <collin@bachman.io>
2022-03-01 17:31:34 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# anime.bcmct.sh
+
+For now this is just a Cloudflare Worker to fetch a random anime from my Planning list, to be added to the On Deck list
---
+++ index.js
@@ -0,0 +1,103 @@
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event));
+});
+
+addEventListener("scheduled", event => {
+  event.waitUntil(handleScheduled(event));
+});
+
+async function handleRequest(event) {
+  if (event.request.method === "GET") {
+    let commitToWatch = await KV.get("commit-to-watch");
+    return new Response(commitToWatch, {
+      headers: { "content-type": "application/json" },
+    });
+  } else if (event.request.method === "POST") {
+    const { headers } = event.request;
+    const contentType = headers.get("content-type") || "";
+    if (contentType.includes("application/json")) {
+      const body = await event.request.json();
+      let response = "";
+      if (body.secret === REFRESH_SECRET) {
+        await fetchAnime();
+        response = ["Success"];
+      } else {
+        response = ["Failure, bad secret"];
+      }
+      return new Response(JSON.stringify(response), {
+        headers: { "content-type": "application/json" },
+      });
+    } else {
+      return new Response("Request was not JSON", {
+        headers: { "content-type": "application/json" },
+      });
+    }
+  }
+}
+
+async function handleScheduled(event) {
+  await fetchAnime();
+}
+
+async function fetchAnime() {
+  const gql = `
+  query {
+  MediaListCollection(userId: 122750, type: ANIME sort: STATUS) {
+    lists {
+      name
+      entries {
+        media {
+          coverImage {
+            medium
+            large
+          }
+          title {
+            native
+            romaji
+            english
+          }
+          description
+          episodes
+          duration
+          format
+          averageScore
+          studios {
+            edges {
+              id
+              isMain
+              node {
+                name
+              }
+            }
+          }
+          siteUrl
+        }
+      }
+    }
+  }
+}
+  `;
+  let planning = null;
+  const response = await fetchGraphQL(gql);
+  response.data.MediaListCollection.lists.forEach(list => {
+    if (list.name === "Planning") {
+      planning = list.entries;
+    }
+  });
+  if (planning !== null) {
+    const randomIndex = Math.floor(Math.random() * planning.length);
+    await KV.put("commit-to-watch", JSON.stringify(planning[randomIndex]));
+  } else {
+    await KV.put("commit-to-watch", JSON.stringify(null));
+  }
+}
+
+async function fetchGraphQL(query) {
+  return fetch("https://graphql.anilist.co/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query }),
+  }).then(response => {
+    return response.json();
+  });
+}
